a7510c1b8b73d00149b86944f2c8a2dd84ce9253
DiamondYuan <admin@diamondyuan.com>
2019-08-16 00:26:10 +0800

    feat: remove previous code and install sass and bootstrap

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-module.exports = {
+const withSass = require('@zeit/next-sass');
+
+module.exports = withSass({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty'
-    }
+      fs: 'empty',
+    };
 
-    return config
-  }
-}
+    return config;
+  },
+});
