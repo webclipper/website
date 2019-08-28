362e7f9b85fb43076304b965393092f7414e271f
DiamondYuan <admin@diamondyuan.com>
2019-08-28 08:27:30 +0800

    feat: add main content

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,18 @@ const Home = () => {
   return (
     <React.Fragment>
       <Head>
-        <script type="text/javascript" src="https://at.alicdn.com/t/font_1373112_0ethyx2l5nn4.js" />
+        <script type="text/javascript" src="https://at.alicdn.com/t/font_1373112_v52xqozph9.js" />
       </Head>
+      <div className={'main'}>
+        <h1>Web Clipper</h1>
+        <p className={'description'}>Now you can save any page on the web to Everywhere</p>
+      </div>
       <div className="brands">
         <h5>
           <strong>Ability</strong> to be compatible with <strong>all</strong> platforms.
         </h5>
         <div className="brand-icons">
-          {['github', 'yuque', 'youdao'].map(type => (
+          {['github', 'yuque', 'youdao', 'notion'].map(type => (
             <Icon key={type} type={type}></Icon>
           ))}
         </div>
