d7164916b8fed650f51fdf4d7e19b3a300e8ad58
DiamondYuan <admin@diamondyuan.com>
2019-09-01 02:49:44 +0800

    feat: remove useless js and css

--- pages/_document.js
+++ pages/_document.js
@@ -14,8 +14,6 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <link rel="stylesheet" href="/static/plugins/bootstrap-4.3.1/css/bootstrap.min.css" />
-          <link rel="stylesheet" href="/static/plugins/slick-1.8.1/slick.css" />
-          <link rel="stylesheet" href="/static/plugins/meanmenu/meanmenu.css" />
           <link rel="stylesheet" href="/static/fonts/ep-icon-fonts/css/ep-font.css" />
           <link rel="stylesheet" href="/static/fonts/fontawesome-5/css/all.min.css" />
           <link rel="stylesheet" href="/static/css/settings.css" />
@@ -25,12 +23,6 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
           <script src={iconFontUrl}></script>
-          <script src="/static/plugins/jquery/jquery.min.js"></script>
-          <script src="/static/plugins/jquery/jquery-migrate.min.js"></script>
-          <script src="/static/plugins/bootstrap-4.3.1/js/bootstrap.bundle.js"></script>
-          <script src="/static/plugins/slick-1.8.1/slick.min.js"></script>
-          <script src="/static/plugins/meanmenu/jquery.meanmenu.js"></script>
-          <script src="/static/js/active.js"></script>
         </body>
       </html>
     );
