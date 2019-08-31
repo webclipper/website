2630509a301cd6ba255146d579d983b4a6f119de
DiamondYuan <admin@diamondyuan.com>
2019-09-01 02:59:28 +0800

    feat: 把页面各个部分抽离成组件

---
+++ components/Cta/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { ExtensionUrl } from '../../common';
+
+export default () => (
+  <section className="cta-section" style={{ marginTop: 100 }}>
+    <div className="container">
+      <div className="cta-inner-wrapper">
+        <div className="row justify-content-center">
+          <div className="col-xl-7 col-lg-8">
+            <div className="cta-content">
+              <span className="title-small-text">Ready to take action?</span>
+              <h2>Try our free software!</h2>
+              <p>
+                We designed and tested prototypes that helped identify pain points in the account
+                creation process. Together, we shaped the new standard.
+              </p>
+              <div className="cta-btn">
+                <a
+                  href={ExtensionUrl}
+                  className="btn btn-outlined--white btn-with-icon btn-height-60"
+                >
+                  Get Started
+                  <span
+                    style={{
+                      color: '#cdf4ec;',
+                      opacity: 0.8,
+                      marginLeft: 2,
+                      fontWeight: 400,
+                      fontSize: 16,
+                    }}
+                  >
+                    It's free
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
---
+++ components/Feature/index.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+
+export default () => (
+  <section className="feature-section">
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-8">
+          <div className="section-title">
+            <h2>One Extension, Every Solution</h2>
+            <p>
+              We designed and tested prototypes that helped identify pain points in the account
+              creation process. Together, we shaped the new standard.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-lg-10">
+          <div className="row mb-d-30">
+            <div className="col-sm-6 mb--30">
+              <div className="feature-widget">
+                <div className="widget-icon">
+                  <i className="icon icon-edit-72" />
+                </div>
+                <div className="content">
+                  <h5>Easy to Use</h5>
+                  <p>
+                    Whether it’s a small internal app or a new for millions of customers, our design
+                    and development teams.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 mb--30">
+              <div className="feature-widget">
+                <div className="widget-icon">
+                  <i className="icon icon-layers-3" />
+                </div>
+                <div className="content">
+                  <h5>300+ Blocks</h5>
+                  <p>
+                    Whether it’s a small internal app or a new for millions of customers, our design
+                    and development teams.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 mb--30">
+              <div className="feature-widget">
+                <div className="widget-icon">
+                  <i className="icon icon-tablet-mobile" />
+                </div>
+                <div className="content">
+                  <h5>100% Responsive</h5>
+                  <p>
+                    Whether it’s a small internal app or a new for millions of customers, our design
+                    and development teams.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 mb--30">
+              <div className="feature-widget">
+                <div className="widget-icon">
+                  <i className="icon icon-book-open-2" />
+                </div>
+                <div className="content">
+                  <h5>Rich Documentation</h5>
+                  <p>
+                    Whether it’s a small internal app or a new for millions of customers, our design
+                    and development teams.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 mb--30">
+              <div className="feature-widget">
+                <div className="widget-icon">
+                  <i className="icon icon-webpage" />
+                </div>
+                <div className="content">
+                  <h5>50+ New Pages</h5>
+                  <p>
+                    Whether it’s a small internal app or a new for millions of customers, our design
+                    and development teams.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-6 mb--30">
+              <div className="feature-widget">
+                <div className="widget-icon">
+                  <i className="icon icon-l-settings" />
+                </div>
+                <div className="content">
+                  <h5>Quick Setup</h5>
+                  <p>
+                    Whether it’s a small internal app or a new for millions of customers, our design
+                    and development teams.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
