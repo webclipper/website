51d7bca6f68bf1b5a1c37602a2c4effde5b46e65
DiamondYuan <admin@diamondyuan.com>
2019-10-28 14:06:11 +0800

    feat: support guest_email

--- components/Price/index.jsx
+++ components/Price/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.scss';
 import { ExtensionUrl } from '../../common';
+import { withRouter } from 'next/router';
 
 const data = [
   {
@@ -37,74 +38,82 @@ const data = [
   }
 ];
 
-export default () => (
-  <section className='pricing-section'>
-    <div className='container'>
-      <div className='row justify-content-center'>
-        <div className='col-lg-8'>
-          <div className='section-title text-center'>
-            <h2>Buy the Web Clipper Powerpack</h2>
-            <p>All prices listed below are in USD</p>
+const Price = props => {
+  return (
+    <section className='pricing-section'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-lg-8'>
+            <div className='section-title text-center'>
+              <h2>Buy the Web Clipper Powerpack</h2>
+              <p>All prices listed below are in USD</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='custom-tab-wrapper monthly' id='pricing-tab-7'>
-        <div className='row mb-d-30'>
-          {data.map(o => (
-            <div className='col-lg-4'>
-              <div className={`pricing-card ${o.active ? 'active' : ''}`}>
-                <div className='card-head'>
-                  <h6>{o.title}</h6>
-                  {!!o.badge && <span className='card-badge'>Popular</span>}
-                  <h2>
-                    <span className='small-title'>$</span>
-                    <span
-                      className='big-txt'
-                      data-monthly={o.price}
-                      data-yearly={14}
-                    >
-                      {o.price}
-                    </span>
-                    <span className='month' data-monthly={`/${o.unit}`}>
-                      /{o.unit}
-                    </span>
-                  </h2>
-                  <p>{o.description}</p>
-                </div>
-                <div className='card-content'>
-                  <ul className='pricing-list'>
-                    {o.feature.map(fe => (
-                      <li>
-                        <span>
-                          <i className='icon icon-check-2' />
-                        </span>
-                        {fe}
-                      </li>
-                    ))}
-                    {o.featureNotSupported.map(fe => (
-                      <li className='disable-opt'>
-                        <span>
-                          <i className='icon icon-check-2' />
-                        </span>
-                        {fe}
-                      </li>
-                    ))}
-                  </ul>
-                  <div className='card-btn'>
-                    <a
-                      href={o.Href}
-                      className='btn btn--primary'
-                      target='_blank'
-                    >
-                      {o.button}
-                    </a>
+        <div className='custom-tab-wrapper monthly' id='pricing-tab-7'>
+          <div className='row mb-d-30'>
+            {data.map(o => (
+              <div className='col-lg-4'>
+                <div className={`pricing-card ${o.active ? 'active' : ''}`}>
+                  <div className='card-head'>
+                    <h6>{o.title}</h6>
+                    {!!o.badge && <span className='card-badge'>Popular</span>}
+                    <h2>
+                      <span className='small-title'>$</span>
+                      <span
+                        className='big-txt'
+                        data-monthly={o.price}
+                        data-yearly={14}
+                      >
+                        {o.price}
+                      </span>
+                      <span className='month' data-monthly={`/${o.unit}`}>
+                        /{o.unit}
+                      </span>
+                    </h2>
+                    <p>{o.description}</p>
+                  </div>
+                  <div className='card-content'>
+                    <ul className='pricing-list'>
+                      {o.feature.map(fe => (
+                        <li>
+                          <span>
+                            <i className='icon icon-check-2' />
+                          </span>
+                          {fe}
+                        </li>
+                      ))}
+                      {o.featureNotSupported.map(fe => (
+                        <li className='disable-opt'>
+                          <span>
+                            <i className='icon icon-check-2' />
+                          </span>
+                          {fe}
+                        </li>
+                      ))}
+                    </ul>
+                    <div className='card-btn'>
+                      <a
+                        href={
+                          props.router.query.guest_email
+                            ? `${o.Href}?guest_email=${props.router.query.guest_email}`
+                            : o.Href
+                        }
+                        className='btn btn--primary'
+                        target='_blank'
+                      >
+                        {o.button}
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
+
+export default withRouter(Price);
