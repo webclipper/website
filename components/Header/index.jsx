672aaa98ebc478a3b847122e4d7ad2ecdf1d1abf
DiamondYuan <admin@diamondyuan.com>
2020-09-22 23:55:06 +0800

    :sparkles: update locales

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -43,7 +43,7 @@ const Header = ({ t }) => {
                     });
                   }}
                 >
-                  <a href={ExtensionUrl}>Get Started</a>
+                  <a href={ExtensionUrl}>{t('components-header-started')}</a>
                 </div>
               </div>
             </div>
